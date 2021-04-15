e8f0fe7c782796cd769a740094a28910e06ee61b
NANOUU <59022292+antoniasymeonidou@users.noreply.github.com>
2021-04-15 12:21:16 +0100

    Update trampoline.lua

--- trampoline.lua
+++ trampoline.lua
@@ -1,4 +1,16 @@
--- Inside the trampoline you need to add a configuration file and inside the configuration search Intvalue and set value to 100
-local trampoline = script.Parent
-trampoline.Velocity = Vector3.new(0, trampoline.Configuration.BounceSpeed.Value, 0)
-trampoline.SurfaceGui.Enabled = false
+-- Trampoline part Script (put as a child to the trampoline part)
+function onTouch(hit)
+    local p = game.Players:GetPlayerFromCharacter(hit.Parent)
+    if p then
+        game.ReplicatedStorage.Jump:FireClient(p)
+    end
+end
+script.Parent.Touched:connect(onTouch)
+
+-- Put a RemoteEvent named "Jump" in game.ReplicatedStorage
+
+-- Put the following LocalScript in game.StarterPlayer.StarterCharacterScripts
+local p = game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart")
+game.ReplicatedStorage.Jump.OnClientEvent:Connect(function()
+    p.Velocity = Vector3.new(0, 100, 0)
+end)
