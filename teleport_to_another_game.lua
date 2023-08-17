a577ef26860ec44642ca41bdb99424f701cae9ff
NANOUU <59022292+antoniasymeonidou@users.noreply.github.com>
2023-08-17 08:32:15 +0100

    Create teleport_to_another_game.lua

---
+++ teleport_to_another_game.lua
@@ -0,0 +1,22 @@
+local telePart = script.Parent
+
+local TeleportService = game:GetService('TeleportService')
+
+local placeID = 5020048423
+
+local canTeleport = true
+
+local function otherGame(otherPart)
+
+local player = game.Players:FindFirstChild(otherPart.Parent.Name)
+
+if player and canTeleport then
+
+	canTeleport = false
+
+	TeleportService:Teleport(placeID, player)
+
+end
+end
+
+telePart.Touched:Connect(otherGame)
